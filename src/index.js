fa6bb41027709835611a5a0b02856058fc4c094b
Kushagra Nigam <93257324+TheMarvelFan@users.noreply.github.com>
2024-12-29 15:48:03 +0530

    util code written

--- src/index.js
+++ src/index.js
@@ -1,12 +1,22 @@
 // require('dotenv').config({path: './env'});
 import dotenv from "dotenv";
 import connectToDB from "./db/mongo.js";
+import { app } from "./app.js";
 
 dotenv.config({
   path: "./env"
 });
 
-connectToDB();
+connectToDB()
+  .then(() => {
+    app.listen(process.env.PORT || 8000, () => {
+      console.log(`Server is running on port: ${process.env.PORT || 8000}`);
+    });
+  })
+  .catch((err) => {
+    console.error(`MongoDB Connection failed: ${err}`);
+    throw err;
+  });
 
 /*
 import express from "express";
